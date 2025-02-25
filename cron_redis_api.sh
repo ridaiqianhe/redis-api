4eea7538d298e031c1f9a65fb4823ae60bbcd82b
日代千鹤 <121543611+ridaiqianhe@users.noreply.github.com>
2025-02-26 02:16:26 +0800

    Update cron_redis_api.sh

--- cron_redis_api.sh
+++ cron_redis_api.sh
@@ -5,7 +5,7 @@ CRON_LOG="./logs/cron.log"
 # 确保日志目录存在
 mkdir -p "$(dirname "$CRON_LOG")"
 
-if ! curl -s http://127.0.0.1:16739 > /dev/null; then
+if ! curl -s http://127.0.0.1:16379 > /dev/null; then
     echo "[$TIMESTAMP] 服务不可用，重启 Docker 容器..." >> "$CRON_LOG"
     docker restart redis-api >> "$CRON_LOG" 2>&1
 else
